e04a95b0399157128deceadfda73cb778f1025bf
Antonashed <92579755+Antonashed@users.noreply.github.com>
2025-07-04 12:58:11 +0300

    Fix minor issues and ensure newline

--- bot/payment/txlogger.py
+++ bot/payment/txlogger.py
@@ -1,9 +1,15 @@
 from bot.database.models import Database, DepositLog
 
 
-
-#
 def log_transaction(user_id: int, tx_hash: str, ltc_amount: float, usd_amount: float) -> None:
+    """Save information about a deposit transaction."""
     session = Database().session
-    session.add(DepositLog(user_id=user_id, tx_hash=tx_hash, ltc_amount=ltc_amount, usd_amount = usd_amount))
+    session.add(
+        DepositLog(
+            user_id=user_id,
+            tx_hash=tx_hash,
+            ltc_amount=ltc_amount,
+            usd_amount=usd_amount,
+        )
+    )
     session.commit()
